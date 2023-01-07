c13b8fc8db13390bc2ce56d0588342a36999f64c
matiushariman <matius.hariman@gmail.com>
2023-01-07 10:33:59 +0700

    add unit test for component that uses zustand store

--- apps/zustand-app/src/app/App.spec.tsx
+++ apps/zustand-app/src/app/App.spec.tsx
@@ -1,15 +1,20 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import App from './App';
 
 describe('App', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<App />);
-    expect(baseElement).toBeTruthy();
+  it('should be initialized with 0 bear', () => {
+    render(<App />);
+
+    expect(screen.getByText(/0 bears around here/i)).toBeDefined();
   });
 
-  it('should have a greeting as the title', () => {
-    const { getByText } = render(<App />);
-    expect(getByText(/Welcome zustand-app/gi)).toBeTruthy();
+  it('should allow user to increase bear', async () => {
+    render(<App />);
+
+    await userEvent.click(screen.getByText(/increase bears/i));
+
+    expect(screen.getByText(/1 bears around here/i)).toBeDefined();
   });
 });
--- apps/zustand-app/src/app/BearsDisplay.tsx
+++ apps/zustand-app/src/app/BearsDisplay.tsx
@@ -3,5 +3,5 @@ import { selectBearStore } from './store/useBearStore';
 export default function BearsDisplay() {
   const bears = selectBearStore.useBears();
 
-  return <p>{bears} around here</p>;
+  return <p>{bears} bears around here</p>;
 }
